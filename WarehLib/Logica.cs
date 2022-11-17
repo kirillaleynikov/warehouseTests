85ac203d677f0f1aa668bf484cf713616cbb21e9
kirillaleynikov <106805966+kirillaleynikov@users.noreply.github.com>
2022-11-17 17:13:00 +0300

    Add files via upload

---
+++ WarehLib/Logica.cs
@@ -0,0 +1,27 @@
+﻿using warehousenet2;
+namespace WarehLib
+{
+    public class Logica
+    {
+        private List<Tovar> WareL = new List<Tovar>();
+
+        public Logica() { }
+        public void Add(Tovar argument)
+        {
+            WareL.Add(argument);
+        }
+        public void Remove(Tovar argument)
+        {
+            WareL.Remove(argument);
+        }
+        public void Change(Tovar argument, Tovar id)
+        {
+            var index = WareL.IndexOf(id);
+            WareL[index] = argument;
+        }
+        public List<Tovar> Get()
+        {
+            return WareL;
+        }
+    }
+}
